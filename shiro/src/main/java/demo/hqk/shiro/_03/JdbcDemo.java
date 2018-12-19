b5aa2930e9f9fe3b5beda5e0dbc42e4ab9ee506f
hqk4java <huangblue123@sina.com>
2018-12-19 18:30:06 +0800

    临时文件

---
+++ shiro/src/main/java/demo/hqk/shiro/_03/JdbcDemo.java
@@ -0,0 +1,13 @@
+package demo.hqk.shiro._03;
+
+import org.apache.shiro.realm.text.IniRealm;
+
+/**
+org.apache.shiro.realm.text.IniRealm; 默认
+public static final String USERS_SECTION_NAME = "users";
+public static final String ROLES_SECTION_NAME = "roles";
+*/
+public class JdbcDemo extends IniRealm{
+	 public static final String USERS_SECTION_NAME = "users";
+	
+}
